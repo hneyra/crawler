c2299436066be253608389536d6585983b65b5cd
Jorge <jorge.neyra@gmail.com>
2026-03-27 13:22:45 +0100

    fix: more fixes

--- src/main/java/crawler/discovery/DiscoveryController.java
+++ src/main/java/crawler/discovery/DiscoveryController.java
@@ -4,16 +4,16 @@ import crawler.model.Category;
 import crawler.model.CategoryRepository;
 import crawler.model.Site;
 import crawler.model.SiteRepository;
-import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/discovery")
